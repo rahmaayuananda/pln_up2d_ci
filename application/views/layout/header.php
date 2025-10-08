46d35ba26bcdf8be3524a68af39619d774cafb51
aayyu09 <rahma22ti@mahasiswa.pcr.ac.id>
2025-10-08 16:08:13 +0700

    init commit dashboard

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -31,7 +31,6 @@
 
 <body class="g-sidenav-show bg-gray-100">
   <div class="min-height-300 bg-dark position-absolute w-100"></div>
-
   <!-- Sidebar -->
   <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4" id="sidenav-main">
     <div class="sidenav-header">
