abe8a89d3a3ea1188f61bdfe2a6a63b52dae4f99
aayyu09 <rahma22ti@mahasiswa.pcr.ac.id>
2025-10-09 21:03:57 +0700

    htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://pln_up2d_ci.test/';
+$config['base_url'] = 'http://localhost/pln_up2d_ci/';
 
 /*
 |--------------------------------------------------------------------------
