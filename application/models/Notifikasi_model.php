674bba62b1145c59e235156b8f7370ca1b91ba39
Ayip17 <arifarrafi17@gmail.com>
2025-11-04 10:46:17 +0700

    Refactor Notifikasi_model

--- application/models/Notifikasi_model.php
+++ application/models/Notifikasi_model.php
@@ -3,42 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Notifikasi_model extends CI_Model
 {
-    private $table = 'notifikasi';
-
-    public function get_all_notifications()
-    {
-        $this->db->order_by('CREATED_AT', 'DESC');
-        return $this->db->get($this->table)->result_array();
-    }
-
-    public function insert_notification($data)
-    {
-        return $this->db->insert($this->table, $data);
-    }
-
-    public function update_notification($id, $data)
-    {
-        $this->db->where('ID_NOTIF', $id);
-        return $this->db->update($this->table, $data);
-    }
-
-    public function mark_all_read()
-    {
-        return $this->db->update($this->table, ['IS_READ' => 1, 'READ_AT' => date('Y-m-d H:i:s')]);
-    }
-
-    public function get_unread_count()
-    {
-        $this->db->where('IS_READ', 0);
-        $this->db->from($this->table);
-        return $this->db->count_all_results();
-    }
-}
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
-class Notifikasi_model extends CI_Model
-{
     private $table = 'log_aktivitas';
 
     public function __construct()
@@ -46,14 +10,27 @@ class Notifikasi_model extends CI_Model
         parent::__construct();
     }
 
-    // Ambil semua notifikasi
+    /**
+     * Ambil semua notifikasi
+     */
     public function get_all()
     {
         $this->db->order_by('tanggal', 'DESC');
         return $this->db->get($this->table)->result_array();
     }
 
-    // Ambil notifikasi terbaru (misal 5 terakhir)
+    /**
+     * Ambil semua notifikasi (alias untuk kompatibilitas)
+     */
+    public function get_all_notifications()
+    {
+        return $this->get_all();
+    }
+
+    /**
+     * Ambil notifikasi terbaru
+     * @param int $limit Jumlah data yang diambil
+     */
     public function get_latest($limit = 5)
     {
         $this->db->order_by('tanggal', 'DESC');
@@ -61,7 +38,13 @@ class Notifikasi_model extends CI_Model
         return $this->db->get($this->table)->result_array();
     }
 
-    // Tambah log aktivitas
+    /**
+     * Tambah log aktivitas / notifikasi
+     * @param string $aksi Jenis aksi (INSERT, UPDATE, DELETE)
+     * @param string $tabel Nama tabel yang dimodifikasi
+     * @param string $user Username yang melakukan aksi
+     * @param string $keterangan Keterangan tambahan
+     */
     public function tambah_log($aksi, $tabel, $user, $keterangan = null)
     {
         $data = [
@@ -75,17 +58,64 @@ class Notifikasi_model extends CI_Model
         $this->db->insert($this->table, $data);
     }
 
-    // Hitung notifikasi belum dibaca
+    /**
+     * Insert notifikasi (alias untuk kompatibilitas)
+     */
+    public function insert_notification($data)
+    {
+        if (isset($data['aksi']) && isset($data['tabel']) && isset($data['user'])) {
+            return $this->tambah_log(
+                $data['aksi'],
+                $data['tabel'],
+                $data['user'],
+                $data['keterangan'] ?? null
+            );
+        }
+        return $this->db->insert($this->table, $data);
+    }
+
+    /**
+     * Update notifikasi
+     * @param int $id ID notifikasi
+     * @param array $data Data yang akan diupdate
+     */
+    public function update_notification($id, $data)
+    {
+        $this->db->where('id', $id);
+        return $this->db->update($this->table, $data);
+    }
+
+    /**
+     * Hitung notifikasi belum dibaca
+     */
     public function count_unread()
     {
         $this->db->where('status', 0);
         return $this->db->count_all_results($this->table);
     }
 
-    // Tandai semua sudah dibaca
-    public function mark_as_read()
+    /**
+     * Hitung notifikasi belum dibaca (alias untuk kompatibilitas)
+     */
+    public function get_unread_count()
+    {
+        return $this->count_unread();
+    }
+
+    /**
+     * Tandai semua notifikasi sebagai sudah dibaca
+     */
+    public function mark_all_read()
     {
         $this->db->set('status', 1);
-        $this->db->update($this->table);
+        return $this->db->update($this->table);
+    }
+
+    /**
+     * Tandai semua notifikasi sebagai sudah dibaca (alias)
+     */
+    public function mark_as_read()
+    {
+        return $this->mark_all_read();
     }
 }
